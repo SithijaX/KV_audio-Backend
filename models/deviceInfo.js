80dc2cb6f0751ff3a966dccf3d75e9ced4ed4ac6
SithijaX <sithijax2@gmail.com>
2025-08-06 21:25:29 +0530

    Refactor device schema to include detailed fields and ensure unique key constraint

--- models/deviceInfo.js
+++ models/deviceInfo.js
@@ -1,10 +1,41 @@
 import mongoose from "mongoose";
 
 const deviceSchema = mongoose.Schema({
-    id : String,
-    name : String,
-    Availability : Boolean,
-    Price : Number
+    key:{
+        type : String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    Availability: {
+        type: String,
+        required: true,
+        default: "In Stock"
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    demensions: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        default: "any"
+    }
 })
 
 //create a model
