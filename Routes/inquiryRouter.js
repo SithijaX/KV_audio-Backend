0093c13dd57b5327ed5ff938d2dfbeacf013dc6b
SithijaX <sithijax2@gmail.com>
2025-08-09 22:04:04 +0530

    Add delete inquiry route to inquiryRouter

--- Routes/inquiryRouter.js
+++ Routes/inquiryRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addInquiry, viewInquiries } from "../Controllers/inquiryController.js";
+import { addInquiry, viewInquiries, deleteInquiry } from "../Controllers/inquiryController.js";
 
 const router = express.Router();
 
 router.post("/add", addInquiry);
 router.get("/inquiries", viewInquiries);
+router.delete("/delete", deleteInquiry);
 
 export default router;
